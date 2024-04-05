83bd71fba0a202e3f72da8bd783fc8e3be4a862d
XenaSit <sitnikovaxena@gmail.com>
2024-04-05 17:48:25 -0400

    python notes for the lecture

---
+++ wk03-10is_even.py
@@ -0,0 +1,17 @@
+def is_even(num):
+    '''(int) -> bool
+
+    return whether num is even
+
+    >>> is_even(4)
+    True
+    >>> is_even(7)
+    False
+    '''
+    
+    return num % 2 == 0
+
+##    if num % 2 == 0:
+##        return True
+##    else:
+##        return False
