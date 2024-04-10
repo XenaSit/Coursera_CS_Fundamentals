026685c27308c254fe8832bba464255c086a83ee
XenaSit <sitnikovaxena@gmail.com>
2024-04-10 19:52:13 -0400

    Test for week 4

---
+++ wk04-06def.py
@@ -0,0 +1,8 @@
+find and use functions from within modules
+for example module Math contains functions that are related to Math such as square root and factorial
+
+values, also known as objects, can contain functions
+functions inside of objects are called methods
+form of method call: Object.method(arguments)
+
+To use a function that has been defined inside module Math,, we first import math
